bb4824c801f1f3bb8d712b28b8ac97a8eb09c886
German <magicnova@hotmail.com>
2018-01-15 01:00:38 -0300

    now u can edit stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -14,3 +14,23 @@ exports.createStore = async (req,res)=>{
 	req.flash('success',`Successfully created ${store.name}. Care to leave a review?`)
 	res.redirect(`/stores/${store.slug}`);
 }
+
+exports.getStores =async(req,res)=>{
+	const stores = await Store.find();
+	res.render('stores',{title: 'Stores',stores })
+}
+
+exports.editStore= async(req,res) =>{
+	const store = await Store.findOne({_id:req.params.id});
+	res.render('editStore',{title: `Edit ${store.name}`,store})
+}
+
+exports.updateStore = async (req,res)=>{
+	const options = {
+		new:true, //return the new store instead of the ol done
+		runValidators:true,
+	}
+	const store = await Store.findOneAndUpdate({_id:req.params.id},req.body,options).exec();
+	req.flash('success',`Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}>"View Store!</a>`)
+	res.redirect(`/stores/${store._id}/edit`);
+}
