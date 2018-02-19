b89124a1a1884e4721b3d94af860e9b0d42d5c99
German <magicnova@hotmail.com>
2018-02-19 16:16:00 -0300

    added pagination

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -50,8 +50,25 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
-    const stores = await Store.find();
-    res.render("stores", { title: "Stores", stores });
+    const page = req.params.page || 1;
+    const limit = 5;
+    const skip = page * limit - limit;
+    const storesPromise = Store.find()
+        .skip(skip)
+        .limit(limit)
+        .sort({ created: "desc" });
+
+    const countPromise = Store.count();
+
+    const [stores, count] = await Promise.all([storesPromise, countPromise]);
+    const pages = Math.ceil(count / limit);
+    if (!stores.length && skip) {
+        req.flash("info", `You asked for page ${page}.But doesn't exist. So i put you on page ${pages}`);
+        res.redirect(`/stores/page/${pages}`);
+        return;
+    }
+
+    res.render("stores", { title: "Stores", stores, page, pages, count });
 };
 
 exports.editStore = async (req, res) => {
